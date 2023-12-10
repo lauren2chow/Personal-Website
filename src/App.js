9c2d9c5ca480a010b6388ae87c70f744707be78d
lauren2chow <117493200+lauren2chow@users.noreply.github.com>
2023-12-10 06:43:16 -0800

    updated the favicon and table title

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
 import { ScrollToTopButton } from "./components/ScrollToTopButton";
+import {Helmet} from "react-helmet";
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Lauren Chow</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="icon and title" />
+      </Helmet>
       <NavBar />
       <Footer />
       <ScrollToTopButton />
